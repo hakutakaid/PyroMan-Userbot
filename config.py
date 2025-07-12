24d7badce905bd6919cb05ad10a823435ed80e47
hakutakaid <hakutakaid@users.noreply.github.com>
2025-07-13 01:47:48 +0700

    Auto commit on 2025-07-13 01:47:48 #22658

--- config.py
+++ config.py
@@ -14,15 +14,15 @@ from dotenv import load_dotenv
 load_dotenv("config.env")
 
 
-API_HASH = getenv("API_HASH")
-API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "7c4bdc0ad4355f8a934701af9d463d20")
+API_ID = int(getenv("API_ID", "12831328"))
 BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
 if not BLACKLIST_CHAT:
     BLACKLIST_CHAT = [-1001473548283]
 BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
 BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
 BOT_VER = "0.2.0@main"
-BRANCH = getenv("BRANCH", "main")
+BRANCH = getenv("BRANCH", "master")
 CHANNEL = getenv("CHANNEL", "Lunatic0de")
 CMD_HANDLER = getenv("CMD_HANDLER", ".")
 DB_URL = getenv("DATABASE_URL", "")
@@ -31,10 +31,10 @@ GROUP = getenv("GROUP", "SharingUserbot")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
-REPO_URL = getenv("REPO_URL", "https://github.com/mrismanaziz/PyroMan-Userbot")
+REPO_URL = getenv("REPO_URL", "https://github.com/hakutakid/PyroMan-Userbot")
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
 STRING_SESSION5 = getenv("STRING_SESSION5", "")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5936236659").split()))
